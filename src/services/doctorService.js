3ec58dfff1889ea279f51ac18285ff69768d71ad
trankhai1478 <trankhai1478@gmail.com>
2022-07-09 16:46:38 +0700

    81

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -140,7 +140,19 @@ let getDetailDoctorById = (inputId)=>{
                         {model: db.Markdown,
                              attributes: ['description', 'contentHTML','contentMarkdown']
                         },
-                        {model: db.Allcode, as:'positionData', attributes: ['valueEn','valueVi'] },                    
+                        {model: db.Allcode, as:'positionData', attributes: ['valueEn','valueVi'] },   
+
+                        {model: db.Doctor_Infor,
+                            attributes:{
+                                exclude: ['id', 'doctorId']
+                            },
+                            include: [
+                                {model: db.Allcode, as:'priceTypeData', attributes: ['valueEn','valueVi'] }, 
+                                {model: db.Allcode, as:'provinceTypeData', attributes: ['valueEn','valueVi'] }, 
+                                {model: db.Allcode, as:'paymentTypeData', attributes: ['valueEn','valueVi'] }, 
+
+                            ]                   
+                       },                
                     ],
                     raw : false,
                     nest: true         
@@ -240,11 +252,49 @@ let getScheduleByDate = (doctorId, date) =>{
         }
     })
 }
+let getExtraInforDoctorById = (idInput) =>{
+    return new Promise (async(resolve,reject)=>{
+        try{
+            if(!idInput){
+                resolve({
+                    errCode:1,
+                    errMessage:'Missing required parameters'
+                })
+            }else{
+                let data = await db.Doctor_Infor.findOne({
+                    where: {
+                        doctorId: idInput
+                    },
+                    attributes:{
+                        exclude: ['id', 'doctorId'] //bỏ thuộc tính
+                    },
+                    include: [
+                        {model: db.Allcode, as:'priceTypeData', attributes: ['valueEn','valueVi'] }, 
+                        {model: db.Allcode, as:'provinceTypeData', attributes: ['valueEn','valueVi'] }, 
+                        {model: db.Allcode, as:'paymentTypeData', attributes: ['valueEn','valueVi'] }, 
+
+                    ],
+                    raw : false,
+                    nest: true     
+                })
+                if(!data) data={};
+                resolve({
+                    errCode:0,
+                    data:data
+                })
+                
+            }
+        }catch(e){
+            reject(e);
+        }
+    })
+}
 module.exports={
     getTopDoctorHome:getTopDoctorHome,
     getAllDoctors:getAllDoctors,
     saveDetailInforDoctor:saveDetailInforDoctor,
     getDetailDoctorById:getDetailDoctorById,
     bulkCreateSchedule:bulkCreateSchedule,
-    getScheduleByDate:getScheduleByDate
+    getScheduleByDate:getScheduleByDate,
+    getExtraInforDoctorById:getExtraInforDoctorById
 } 
