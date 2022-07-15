cd873db0c49ad9e79d6739310f7005b677cf3ac2
trankhai1478 <trankhai1478@gmail.com>
2022-07-15 14:27:19 +0700

    89 hoan thanh token email

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -12,6 +12,20 @@ let  postBookAppointment = async(req, res) =>{
         })
     }
 }
+let  postVerifyBookAppointment = async(req, res) =>{
+    try{
+        let infor = await patientService.postVerifyBookAppointment(req.body); //query khi url ?id ->get , body khi khong co url->posy
+        return res.status(200).json(infor)
+
+    }catch(e){
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
 module.exports = {
-    postBookAppointment:postBookAppointment
+    postBookAppointment:postBookAppointment,
+    postVerifyBookAppointment:postVerifyBookAppointment
 }
--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,6 +1,14 @@
+
 import db from "../models/index";
 require('dotenv').config();
 import emailService from './emailService';
+import { v4 as uuidv4 } from 'uuid';
+let buildUrlEmail = (doctorId,token)=>{
+
+    let result = `${process.env.URL_REACT}/verify-booking?token=${token}&doctorId=${doctorId}`
+    return result;
+   
+}
 
 let postBookAppointment = (data)=>{
     return new Promise(async(resolve,reject)=>{
@@ -10,14 +18,16 @@ let postBookAppointment = (data)=>{
                 errCode:1,
                 errMessage:'Missing paremeter'
                 })
-            }else{             
+            }else{   
+                let token =  uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+          
                 await emailService.sendSimpleEmail({
                     receiverEmail:data.email,
                     patientName: data.fullName,
                     time:data.timeString,
                     doctorName:data.doctorName,
                     language:data.language,
-                    redirectLink:'https://www.facebook.com/KhaiTran.TQK'
+                    redirectLink: buildUrlEmail(data.doctorId,token)
                 })
                 //cap nhat benh nhat
                  let user = await db.User.findOrCreate({
@@ -35,7 +45,8 @@ let postBookAppointment = (data)=>{
                             doctorId:data.doctorId,
                             patientId: user[0].id,
                             date: data.date,
-                            timeType: data.timeType
+                            timeType: data.timeType,
+                            token:token
                         }
                       
                     })
@@ -52,6 +63,42 @@ let postBookAppointment = (data)=>{
         }
     })
 }
+let postVerifyBookAppointment = (data)=>{
+    return new Promise (async(resolve, reject)=>{
+        try{
+            if(!data.doctorId || !data.token){
+                resolve({
+                errCode:1,
+                errMessage:'Missing paremeter'
+                })
+            }else{   
+                let appointment = await db.Booking.findOne({
+                    where: {doctorId: data.doctorId,
+                        token: data.token,
+                        statusId: 'S1'
+                    },
+                    raw: false // trả ra cli object
+                })
+                if(appointment){
+                    appointment.statusId='S2';
+                    await appointment.save();
+                    resolve({
+                        errCode:0,
+                        errMessage:'Update appointment succeed !'
+                    })
+                }else{
+                    resolve({
+                        errCode:2,
+                        errMessage:'Appointment confirmed or does not exsit !'
+                    })
+                }
+            }
+        }catch(e){
+            reject(e);
+        }
+    })
+}
 module.exports = {
-    postBookAppointment:postBookAppointment
+    postBookAppointment:postBookAppointment,
+    postVerifyBookAppointment:postVerifyBookAppointment
 }
